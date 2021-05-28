82100e905ccb9a33cf8bea992df25cbcfd4a06e7
Victor Volkov <vityan2504@gmail.com>
2021-05-28 21:20:11 +0300

    добавление нового функционала(script.js) создал переменные и вывел их значение в консоль, вывел в консоль длину переменной addxpenses, вывел в консоль сообщения, привёл переменную addxpenses к нижнему регистру и разбил строку на массив, создал переменную budgetay и вывел в консоль

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const money = 50,
+const money = 50000,
    income = 'Фриланс',
-   addExpenses = 'Интернет, продукты, коммуналка',
+   addExpenses = 'Интернет, Продукты, Коммуналка',
    deposit = false,
    mission = 300000,
    period = 12;
@@ -8,3 +8,14 @@ const money = 50,
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} рублей`);
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+const budgetDay = money / 30;
+
+console.log(budgetDay);
