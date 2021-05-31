8d980c9d9f3efeb42f9922a18bb2eacea615e5c6
Victor Volkov <vityan2504@gmail.com>
2021-05-31 20:29:53 +0300

    добавление ( script.js ) добавил проверку if,elsу

--- hard/script.js
+++ hard/script.js
@@ -1,6 +1,6 @@
 let num = 266219;
-num = num.toString();
 let sum = 1;
+num = num.toString();
 
 for (let i = 0; i < num.length; i++) {
    sum *= +num[i];
@@ -13,3 +13,32 @@ sum = sum**3;
 sum = sum.toString();
 
 console.log(sum.slice(0,2));
+
+/////////////////////////////////
+
+const lang = prompt('ru или en');
+
+
+if(lang === 'ru') {
+   console.log([
+      'Понедельник', 
+      'вторник', 
+      'cреда', 
+      'четверг', 
+      'пятница', 
+      'суббота', 
+      'воскресенье'
+   ]);
+} else if (lang === 'en'){
+   console.log([
+      'monday',
+      'tuesday', 
+      'wednesday',
+      'thursday ',
+      'friday ',
+      'saturday ',
+      'sunday' 
+      ]);
+} else {
+   console.log('Ошибка');
+}
