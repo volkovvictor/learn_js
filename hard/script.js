915baffce0891f030582dfacc032c03357f6935f
Victor Volkov <vityan2504@gmail.com>
2021-06-02 22:40:15 +0300

    добавление ( script.js ) добавил функцию upgradestring, которая чистит строку и добавляет троеточиe

--- hard/script.js
+++ hard/script.js
@@ -1,122 +1,17 @@
 'use strict';
 
-let num = 266219;
-let sum = 1;
-num = num.toString();
+const inputString = prompt("Введите строку"),
+      upgradeString = function(data) {
+         if(!isNaN(data)) {
+            console.log('Ошибка! Введите строку');
+         } else {
+            data = data.trim();
+            if(data.length > 30) {
+               data = data.slice(0, 30) + '...';
+            }
+         }
 
-for (let i = 0; i < num.length; i++) {
-   sum *= +num[i];
-}
+         return data;
+      };
 
-console.log(sum);
-
-sum = sum**3;
-
-sum = sum.toString();
-
-console.log(sum.slice(0,2));
-
-/////////////////////////////////
-
-const lang = prompt('ru или en');
-const arr = [
-   [
-      'Понедельник', 
-      'вторник', 
-      'cреда', 
-      'четверг', 
-      'пятница', 
-      'суббота', 
-      'воскресенье'
-   ], 
-   [
-      'monday',
-      'tuesday', 
-      'wednesday',
-      'thursday ',
-      'friday ',
-      'saturday ',
-      'sunday' 
-   ]
-];
-
-if(lang === 'ru') {
-   console.log([
-      'Понедельник', 
-      'вторник', 
-      'cреда', 
-      'четверг', 
-      'пятница', 
-      'суббота', 
-      'воскресенье'
-   ]);
-} else if (lang === 'en'){
-   console.log([
-      'monday',
-      'tuesday', 
-      'wednesday',
-      'thursday ',
-      'friday ',
-      'saturday ',
-      'sunday' 
-      ]);
-} else {
-   console.log('Ошибка');
-}
-
-switch(lang)  {
-   case 'ru': 
-      console.log([
-      'Понедельник', 
-      'вторник', 
-      'cреда', 
-      'четверг', 
-      'пятница', 
-      'суббота', 
-      'воскресенье'
-      ]);
-   break;
-
-   case 'en':
-      console.log([
-         'monday',
-         'tuesday', 
-         'wednesday',
-         'thursday ',
-         'friday ',
-         'saturday ',
-         'sunday' 
-         ]);
-      break;
-
-   default: console.log('Ошибка');
-}
-
-lang === 'ru' ? console.log(arr[0]) : console.log(arr[1]);
-
-///////////////////////////////////////////////////////////
-
-const namePerson = prompt("Имя");
-
-if(namePerson === 'Артём') {
-   console.log('директор');
-} else if (namePerson === 'Максим') {
-   console.log('преподаватель');
-} else {
-   console.log('студент');
-}
-
-switch(namePerson) {
-   case 'Артём':
-      console.log('директор');
-      break;
-   case 'Максим':
-      console.log('преподаватель');
-      break;
-   default: 
-      console.log('студент');
-}
-
-
-namePerson === 'Артём' ? console.log('директор') :
-namePerson === 'Максим' ? console.log('преподаватель') : console.log('студент');
+console.log(upgradeString(inputString));
