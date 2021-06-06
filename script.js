8b99bacc3e95f80339d33c6bea4467a11f2986df
Victor Volkov <vityan2504@gmail.com>
2021-06-06 20:43:51 +0300

    perf (script.js) создал объект appData и переписал переменные в качестве его свойств и методов

--- script.js
+++ script.js
@@ -15,17 +15,31 @@ const isNumber = function (n) {
 
 start(money);
 
-const income = 'Фриланс',
-      addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
-      deposit = confirm('Есть ли у вас депозит в банке?'),
-      mission = 300000,
-      period = 12,
-      expenses = [],
-      getExpensesMonth = function() {
+const appData = {
+   income: {}, 
+   addIncome: [], 
+   expenses: {}, 
+   addExpenses: [], 
+   deposit: false, 
+   mission: 300000, 
+   period: 12, 
+   budget: money, 
+   budgetDay: 0, 
+   budgetMonth : 0, 
+   expensesMonth: 0, 
+   asking: function() { 
+      const addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'); 
+
+      appData.addExpenses = addExpenses.toLowerCase().split(', '); 
+      appData.deposit = confirm('Есть ли у вас депозит в банке?'); 
+   },
+};
+
+   appData.getExpensesMonth = function() {
          let sum = 0;
 
          for(let i = 0; i < 2; i++) {
-            expenses[i] = prompt('Введите обязательную статью расходов?');
+            appData.addExpenses[i] = prompt('Введите обязательную статью расходов?');
             amount = prompt('Во сколько это обойдется?');
 
             while(!isNumber(amount)) {
@@ -35,19 +49,21 @@ const income = 'Фриланс',
             sum += +amount;
          }
          return sum;
-      },
-      expensesAmount = getExpensesMonth(),
-      getAccumulatedMonth = function() {
+      };
+
+const expensesAmount = appData.getExpensesMonth();
+
+appData.getAccumulatedMonth = function() {
          return money - expensesAmount;
-      },
-      accumulatedMonth = getAccumulatedMonth(),
-      getTargetMonth = function() {
-         return Math.ceil(mission / accumulatedMonth);
-      },
-      budgetDay = accumulatedMonth / 30,
-      showTypeOf = function(data) {
-         return typeof(data);
-      },
+      };
+
+const accumulatedMonth = appData.getAccumulatedMonth();
+
+appData.getTargetMonth = function() {
+         return Math.ceil(appData.mission / accumulatedMonth);
+      };
+
+const budgetDay = accumulatedMonth / 30,
       getStatusIncome = function() {
          if(budgetDay >= 1200) {
             return ('У вас высокий уровень дохода');
@@ -60,17 +76,15 @@ const income = 'Фриланс',
          }
       };
 
-console.log(addExpenses.length);
+console.log(appData.addExpenses.length);
 
-console.log(`Период равен ${period} месяцев`);
-console.log(`Цель заработать ${mission} рублей`);
-
-console.log(addExpenses.toLowerCase().split(', '));
+console.log(`Период равен ${appData.period} месяцев`);
+console.log(`Цель заработать ${appData.mission} рублей`);
 
 console.log('Бюджет на месяц: ' + expensesAmount);
 
-if(getTargetMonth() >= 0) {
-   console.log('Цель будет достигнута за ' + getTargetMonth() + ' месяцев(-а)');
+if(appData.getTargetMonth() >= 0) {
+   console.log('Цель будет достигнута за ' + appData.getTargetMonth() + ' месяцев(-а)');
 } else {
    console.log('Цель не будет достигнута');
 }
